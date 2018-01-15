ebd0682605d54aa0b9b5d2af2efc3a888593f4cc
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2018-01-15 21:03:09 +0100

    Add support for vmfs (tests:iscsi,local) datastores

--- lib/ansible/modules/cloud/vmware/vmware_datastore_host.py
+++ lib/ansible/modules/cloud/vmware/vmware_datastore_host.py
@@ -70,6 +70,20 @@ extends_documentation_fragment: vmware.documentation
 '''
 
 EXAMPLES = r'''
+- name: Mount VMFS datastores to ESXi
+  vmware_datastore_host:
+      hostname: '{{ vcenter_hostname }}'
+      username: '{{ vcenter_user }}'
+      password: '{{ vcenter_pass }}'
+      datacenter_name: '{{ datacenter }}'
+      datastore_name: '{{ item.name }}'
+      datastore_type: '{{ item.type }}'
+      vmfs_device_name: 'naa.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+      vmfs_version: 6
+      esxi_hostname: '{{ inventory_hostname }}'
+      state: present
+  delegate_to: localhost
+
 - name: Mount NFS datastores to ESXi
   vmware_datastore_host:
       hostname: '{{ vcenter_hostname }}'
@@ -78,9 +92,9 @@ EXAMPLES = r'''
       datacenter_name: '{{ datacenter }}'
       datastore_name: '{{ item.name }}'
       datastore_type: '{{ item.type }}'
-      datastore_nfs_server: '{{ item.server }}'
-      datastore_nfs_path: '{{ item.path }}'
-      datastore_nfs_ro: no
+      nfs_server: '{{ item.server }}'
+      nfs_path: '{{ item.path }}'
+      nfs_ro: no
       esxi_hostname: '{{ inventory_hostname }}'
       state: present
   delegate_to: localhost
@@ -88,14 +102,13 @@ EXAMPLES = r'''
       - { 'name': 'NasDS_vol01', 'server': 'nas01', 'path': '/mnt/vol01', 'type': 'nfs'}
       - { 'name': 'NasDS_vol02', 'server': 'nas01', 'path': '/mnt/vol02', 'type': 'nfs'}
 
-- name: Remove/Umount NFS datastores from ESXi
+- name: Remove/Umount Datastores from ESXi
   vmware_datastore_host:
       hostname: '{{ vcenter_hostname }}'
       username: '{{ vcenter_user }}'
       password: '{{ vcenter_pass }}'
       datacenter_name: '{{ datacenter }}'
       datastore_name: NasDS_vol01
-      datastore_type: nfs
       esxi_hostname: '{{ inventory_hostname }}'
       state: absent
   delegate_to: localhost
@@ -114,15 +127,17 @@ from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.vmware import connect_to_api, vmware_argument_spec
 
 
-class VMwareNFSDatastoreHost(object):
+class VMwareDatastoreHost(object):
     def __init__(self, module):
         self.module = module
         self.datacenter_name = module.params['datacenter_name']
         self.datastore_name = module.params['datastore_name']
         self.datastore_type = module.params['datastore_type']
-        self.nfs_server = module.params['datastore_nfs_server']
-        self.nfs_path = module.params['datastore_nfs_path']
-        self.nfs_ro = module.params['datastore_nfs_ro']
+        self.nfs_server = module.params['nfs_server']
+        self.nfs_path = module.params['nfs_path']
+        self.nfs_ro = module.params['nfs_ro']
+        self.vmfs_device_name = module.params['vmfs_device_name']
+        self.vmfs_version = module.params['vmfs_version']
         self.esxi_hostname = module.params['esxi_hostname']
         self.state = module.params['state']
         self.content = connect_to_api(module)
@@ -209,29 +224,62 @@ class VMwareNFSDatastoreHost(object):
 
     def mount_datastore_host(self):
         if self.datastore_type == 'nfs':
-            mnt_specs = vim.host.NasVolume.Specification()
-            mnt_specs.remoteHost = self.nfs_server
-            mnt_specs.remotePath = self.nfs_path
-            mnt_specs.localPath = self.datastore_name
-            if self.nfs_ro:
-                mnt_specs.accessMode="readOnly"
-            else:
-                mnt_specs.accessMode="readWrite"
+            self.mount_nfs_datastore_host()
+        if self.datastore_type == 'vmfs':
+            self.mount_vmfs_datastore_host()
+
+
+    def mount_nfs_datastore_host(self):
+        mnt_specs = vim.host.NasVolume.Specification()
+        mnt_specs.remoteHost = self.nfs_server
+        mnt_specs.remotePath = self.nfs_path
+        mnt_specs.localPath = self.datastore_name
+        if self.nfs_ro:
+            mnt_specs.accessMode="readOnly"
         else:
-            self.module.fail_json(msg="Not yet supported datastore type")
+            mnt_specs.accessMode="readWrite"
         error_message_mount = "Cannot mount datastore %s on host %s" % (self.datastore_name, self.esxi_hostname)
         try:
             ds = self.esxi.configManager.datastoreSystem.CreateNasDatastore(mnt_specs)
             if not ds:
                 self.module.fail_json(msg=error_message_mount)
         except vim.fault.DuplicateName:
-            self.module.fail_json(msg=error_message_mount + ": DuplicateName")
+            self.module.fail_json(msg=error_message_mount + ": A datastore with the same name already exists")
+        except vim.fault.AlreadyExists:
+            self.module.fail_json(msg=error_message_mount + ": The local path already exists on the host, or the remote path is already mounted on the host")
         except vim.fault.HostConfigFault:
-            self.module.fail_json(msg=error_message_mount + ": HostConfigFault")
-        except vim.fault.FileNotFound:
-            self.module.fail_json(msg=error_message_mount + ": FileNotFound")
-        except vim.fault.InvalidName:
-            self.module.fail_json(msg=error_message_mount + ": InvalidName")
+            self.module.fail_json(msg=error_message_mount + ": Unable to mount the NAS volume")
+        except vmodl.fault.InvalidArgument:
+            self.module.fail_json(msg=error_message_mount + ": The datastore name is invalid, or the spec is invalid")
+        except vim.fault.NoVirtualNic:
+            self.module.fail_json(msg=error_message_mount + ": VMkernel TCPIP stack is not configured for NFS data")
+        except vim.fault.NoGateway:
+            self.module.fail_json(msg=error_message_mount + ": VMkernel gateway is not configured")
+        except Exception as e:
+            self.module.fail_json(msg=error_message_mount + ": " + str(e))
+        self.module.exit_json(changed=True, result="datastore %s on host %s" % (self.datastore_name, self.esxi_hostname))
+
+
+    def mount_vmfs_datastore_host(self):
+        ds_path = "/vmfs/devices/disks/" + str(self.vmfs_device_name)
+        host_ds_system = self.esxi.configManager.datastoreSystem
+        ds_system = vim.host.DatastoreSystem
+        error_message_mount = "Cannot mount datastore %s on host %s" % (self.datastore_name, self.esxi_hostname)
+        try:
+            vmfs_ds_options = ds_system.QueryVmfsDatastoreCreateOptions(host_ds_system,
+                                                                        ds_path,
+                                                                        self.vmfs_version)
+            vmfs_ds_options[0].spec.vmfs.volumeName = self.datastore_name
+            ds = ds_system.CreateVmfsDatastore(host_ds_system,
+                                               vmfs_ds_options[0].spec)
+        except vim.fault.NotFound:
+            self.module.fail_json(msg=error_message_mount + ": Device is not found")
+        except vim.fault.HostConfigFault:
+            self.module.fail_json(msg=error_message_mount + ": Unable to format the VMFS volume or gather information about the created volume")
+        except vim.fault.DuplicateName:
+            self.module.fail_json(msg=error_message_mount + ": A datastore with the same name already exists")
+        except vmodl.fault.InvalidArgument:
+            self.module.fail_json(msg=error_message_mount + ": The datastore name is invalid, or the spec is invalid")
         except Exception as e:
             self.module.fail_json(msg=error_message_mount + ": " + str(e))
         self.module.exit_json(changed=True, result="datastore %s on host %s" % (self.datastore_name, self.esxi_hostname))
@@ -242,30 +290,37 @@ def main():
     argument_spec.update(
         datacenter_name=dict(type='str', required=True),
         datastore_name=dict(type='str', required=True),
-        datastore_type=dict(type='str', default='present', choices=['nfs', 'vmfs']),
-        datastore_nfs_server=dict(type='str'),
-        datastore_nfs_path=dict(type='str'),
-        datastore_nfs_ro=dict(type='bool', default=False),
+        datastore_type=dict(type='str', choices=['nfs', 'vmfs']),
+        nfs_server=dict(type='str'),
+        nfs_path=dict(type='str'),
+        nfs_ro=dict(type='bool', default=False),
+        vmfs_device_name=dict(type='str'),
+        vmfs_version=dict(type='int'),
         esxi_hostname=dict(type='str', required=True),
         state=dict(type='str', default='present', choices=['absent', 'present'])
     )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
-        supports_check_mode=True
+        supports_check_mode=True,
     )
 
     # more complex required_if
-    if module.params['datastore_type'] == 'nfs' and \
-       not (module.params['datastore_nfs_server'] and module.params['datastore_nfs_path']):
-        msg = "missing datastore_nfs_server or datastore_nfs_path with datastore_type = nfs"
-        module.fail_json(msg=msg)
+    if module.params['state'] == 'present':
+        if module.params['datastore_type'] == 'nfs' and \
+           not (module.params['nfs_server'] and module.params['nfs_path']):
+            msg = "Missing nfs_server or nfs_path with datastore_type = nfs"
+            module.fail_json(msg=msg)
 
+        if module.params['datastore_type'] == 'vmfs' and \
+           not module.params['vmfs_device_name']:
+            msg = "Missing vmfs_device_name with datastore_type = vmfs"
+            module.fail_json(msg=msg)
 
     if not HAS_PYVMOMI:
         module.fail_json(msg='pyvmomi is required for this module')
 
-    vmware_datastore_host = VMwareNFSDatastoreHost(module)
+    vmware_datastore_host = VMwareDatastoreHost(module)
     vmware_datastore_host.process_state()
 
 
